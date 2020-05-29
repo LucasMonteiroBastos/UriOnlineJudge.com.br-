b50de450fa36001322c68432d7e5f2f260699d1b
Lucas Monteiro Bastos <monteiro.bastos1994@gmail.com>
2020-05-29 15:01:31 -0300

    Gasto de Combustivel - 1017

---
+++ uri1017.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1017 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int tempoGasto = sc.nextInt();
+		int velocidadeMeida =sc.nextInt();
+		
+		double resultado = tempoGasto * velocidadeMeida;
+		
+		double distanciaPercorrida = resultado / 12;
+		
+		System.out.printf("%.3f%n",distanciaPercorrida);
+				
+		
+		sc.close();
+	}
+
+}
