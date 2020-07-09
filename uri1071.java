65025afbafa01f1f835761545e7b4af42256b4c1
Lucas Monteiro Bastos <monteiro.bastos1994@gmail.com>
2020-07-08 21:20:50 -0300

    Soma de Impares Consecutivos - 1071

--- uri1071.java
+++ uri1071.java
@@ -4,27 +4,26 @@ public class uri1071 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int x = sc.nextInt();
 		int y = sc.nextInt();
-		
+
 		int min, max;
-		if(x < y) {
+		if (x < y) {
 			min = x;
 			max = y;
-		}
-		else {
+		} else {
 			min = y;
 			max = x;
 		}
-		
+
 		int soma = 0;
-		for(int i=min+1; i<max; i++) {
-			if(i % 2 != 0) {
+		for (int i = min + 1; i < max; i++) {
+			if (i % 2 != 0) {
 				soma = soma + i;
 			}
 		}
-		
+
 		System.out.println(soma);
 		sc.close();
 	}
